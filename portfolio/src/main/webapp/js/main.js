55f1f186b1967e096f0b94b263790bff61553325
Indhu Gunda <indhu.gunda@utexas.edu>
2020-05-27 14:38:17 -0500

    Add header with overlay

--- portfolio/src/main/webapp/js/main.js
+++ portfolio/src/main/webapp/js/main.js
@@ -11,6 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+$(document).ready(function(){
+  $('.header').height($(window).height());
+})
 
 /**
  * Adds a random fact to the page.
