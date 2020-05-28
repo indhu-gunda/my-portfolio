4490abfcfec1e23e18e671e345e693c5e760f9cf
Indhu Gunda <indhu.gunda@utexas.edu>
2020-05-28 15:40:27 -0500

    Scroll to corresponding section when a navigation tab is clicked.

--- portfolio/src/main/webapp/js/main.js
+++ portfolio/src/main/webapp/js/main.js
@@ -13,4 +13,11 @@
 // limitations under the License.
 $(document).ready(function(){
   $('.header').height($(window).height());
+
+  $(".navbar a").click(function(){
+    $("body,html").animate({
+      scrollTop:$("#" + $(this).data('value')).offset().top
+    },1000)
+  
+  })
 })
